70fe1e435e2116d5a9c44cb0a733e724d49fc2fc
Jannydear <jannydear112@gmail.com>
2023-11-27 03:00:56 -0800

    mgt

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,10 @@
-# creates a file in /tmp
-File {  ‘/tmp/school’:
-  Content =>’I love Puppet’,
-  Mode     => ‘0744’,
-  Owner    => ‘www-data’,
-  Group     => ‘www-data’,
+# Using Puppet to create a file in /tmp.
+
+file { '/tmp/school':
+    ensure  => 'file',
+    owner   => 'www-data',
+    group   => 'www-data',
+    mode    => '0744',
+    content => 'I love Puppet',
 }
 
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,9 @@
-# kill process killmenow
+#!/usr/bin/env puppet
+# Using Puppet to create a manifest that kills a process named killmenow.
 
-Exec  {  ‘pkill’:
-   Command  =>  ‘pkill killmenow’,
-   Provider     =>    ‘shell’
+exec { 'kill':
+  command   => 'pkill -f killmenow',
+  path      => ['/usr/bin', '/usr/sbin'],
+  returns   => ['0', '1'],
+  logoutput => true,
 }
-
